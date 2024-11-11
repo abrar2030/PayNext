359e1a0386a07ce40b90a72c7e03cb9aa5ff9abd
abrar2030 <abrarahmedpei@gmail.com>
2024-11-11 15:55:31 +0100

    Auto-commit on 2024-11-11 15:55:29

--- backend/payment-service/src/main/java/PaymentServiceApplication.java
+++ backend/payment-service/src/main/java/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package com.fintech.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.fintech.paymentservice.client")
 public class PaymentServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(PaymentServiceApplication.class, args);
